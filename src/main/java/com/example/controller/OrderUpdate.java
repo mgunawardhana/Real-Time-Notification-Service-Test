2131e80860092cdbbcb67f4d88fb92ed949f5034
mgunawardhana <mrgunawardhana27368@gmail.com>
2025-01-14 12:05:55 +0530

    content update

---
+++ src/main/java/com/example/controller/OrderUpdate.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class OrderUpdate {
+
+    private String orderId;
+    private String status;
+
+}
